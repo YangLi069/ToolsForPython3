56b7ac656d8360a8aebd0fb6285603d5c6f87d20
YangLi069 <piaoxuedeliuyue@163.com>
2021-02-17 15:50:29 +0800

    Update TopOfCount.py
    增加最小堆的方法

--- TopOfCount.py
+++ TopOfCount.py
@@ -15,3 +15,26 @@ heapq.nlargest(3, ((v,k) for k, v in d.items()))
 from collections import Counter
 c = Counter(data)
 print(c.most_common(3))
+
+
+# 方法3：使用最小堆
+from heapq import heappush, heappop
+
+# 用一个最小堆来保存积分最多的100位用户
+# 这样，积分最小的用户刚好在堆顶
+top100 = []
+users = [100, 100, 100, 20, 23, ...]
+
+# 遍历所有用户
+for user in users:
+    # 堆未满100，不断压入
+    if len(top100) < 100:
+        heappush(top100, user)
+        continue
+
+    # 如果当前用户积分比堆中积分最少的用户多，则替换
+    if user > top100[0]:
+        heappop(top100)
+        heappush(top100, user)
+
+# 遍历完毕后，现在堆中保存的用户就是积分最多的100位了
